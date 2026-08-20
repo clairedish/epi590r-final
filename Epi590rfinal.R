8f864d0ff940f6ed0cd846648ab8b45637c0ab6c
Claire Dishman <claire.dishman@emory.edu>
2026-08-20 19:49:11 -0400

    Descriptive table of all characters

--- Epi590rfinal.R
+++ Epi590rfinal.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(gtsummary)
+library(dplyr)
+library (gt)
 
 got <- read_csv(here::here("data","GoT_dataset","character_data_S01-S08.csv"))
 
@@ -22,3 +24,26 @@ got$allegiance_switched <- factor(got$allegiance_switched,
 got$dth_flag <- factor(got$dth_flag,
 											 levels = c(0, 1),
 											 labels = c("No", "Yes"))
+
+#descriptive table
+got %>%
+	select(name, sex, religion, intro_season, dth_flag,
+				 dth_description, dth_season,
+				 exp_episode, social_status, allegiance_last,
+				 allegiance_switched, featured_episode_count) %>%
+	gt() %>%
+	cols_label(
+				 name = "Name",
+				 sex = "Sex",
+				 religion = "Religion",
+				 intro_season = "Season Character First Appeared",
+				 dth_flag = "Charactered Died During Show",
+				 dth_description = "Description of Death",
+				 dth_season = "Season Character Died In",
+				 exp_episode = "Number of Episodes Character Survived",
+				 social_status = "Social Status at Time of Death",
+				 allegiance_last = "Allegiance at Time of Death",
+				 allegiance_switched = "Switched Allegiance During the Show",
+				 featured_episode_count = "How Many Episodes Character Was Featured In"
+						)
+
